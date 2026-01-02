2175e4ec1319c3f37a9cd4329881ad1b6533eeca
Karsten Weng <karsten@weng.eu>
2026-01-02 19:06:09 +0100

    fix(core): resolve base URL for GitHub Pages and custom domain

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,8 +1,1 @@
- <script>
-    (function () {
-      const repo = "mur-art-quiz";
-      const isGitHubPages = location.hostname.endsWith("github.io");
-      const base = isGitHubPages ? `/${repo}/` : "/";
-      document.write(`<base href="${base}">`);
-    })();
-  </script>
+ 
